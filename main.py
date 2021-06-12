e4d993eb92014433a0d93d3358dbd73fd832c30b
C T <85103630+CurtisTrepte@users.noreply.github.com>
2021-06-12 18:40:45 +0000

    Updated underage message

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ def main():
 
   if mainFunction.upper() == "EXIT":
     exit()
-  elif mainFunction.upper() == "WEBSITE":
-    print("curtist.webhostapp.com")
-  elif mainFunction.upper() == ""  
+  elif mainFunction.upper() == "CHICKEN":
+    print("Wassup")  
 
 
 
 #Get name and max
 name = input("What is your name? \n")
-age = int(input("What is your age? \n"))
+age = int(input(f"Hello {name}, what is your age? \n"))
 
 if age < 13:
+  print("Sorry you are not old enough to use Software")
+  exit = input("Please press any key to exit")
   exit()
 else:
   main()
